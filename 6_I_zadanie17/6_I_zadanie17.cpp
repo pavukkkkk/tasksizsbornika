699e5be5b22169fb37e09917f3c843cf5846de51
Konstantin <kirilenkokonstantin2003@gmail.com>
2021-10-31 13:20:09 +0400

    third1

---
+++ 6_I_zadanie17/6_I_zadanie17.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a,g;
+    long long s = 0;
+    cout << "vvedite kolichestvo elementov: ";
+    cin >> n;
+    int* m = new int[n];
+    cout << "vvedite elementi : " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        *(m + i) = a;
+    }
+    cout << "vvedite chislo : " ;
+    cin >> g;
+    for (int i = 0; i < n-1; i++) {
+
+        if (abs((*(m + i)  - *(m + i+1)))==g) {
+            s ++;
+        }
+    }
+    cout << "kolichestvo par : " << s;
+}
+
