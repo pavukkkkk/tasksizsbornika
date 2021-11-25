8389130438a7d5c4dd38fa63d9a71cff4a74d72f
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 01:03:54 +0400

    Update 9_I_zadanie5.cpp

--- 9_I_zadanie5/9_I_zadanie5.cpp
+++ 9_I_zadanie5/9_I_zadanie5.cpp
@@ -1,24 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-#include "fstream";
-#include "string";
+#include "fstream"
+#include "string"
 
 int main()
 {
-	ifstream in("f.txt");
-	string s;
-	int m=0,k = 0,i=0;
-	while (in.peek() != EOF)
-	{
-		i++;
-		getline(in, s);
-		if (m < s.size()) {
-			m = s.size();
-			k = i;
-		}
-		
-	}
-	cout << k;
-	in.close();
-	return 0;
+    ifstream in("f.txt");
+    string s;
+    long int m=0,k = 0,i=0;
+    while (in.peek() != EOF)
+    {
+        i++;
+        getline(in, s);
+        if (m < s.size()) {
+            m = s.length();
+            k = i;
+        }
+        
+    }
+    cout << k;
+    in.close();
+    return 0;
 }
