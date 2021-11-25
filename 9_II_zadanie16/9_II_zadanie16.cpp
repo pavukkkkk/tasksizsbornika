05636df3072ad93c8f064d4326ca37a9f5e613da
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 01:08:02 +0400

    Update 9_II_zadanie16.cpp

--- 9_II_zadanie16/9_II_zadanie16.cpp
+++ 9_II_zadanie16/9_II_zadanie16.cpp
@@ -1,23 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-#include "fstream";
-#include "string";
+#include "fstream"
+#include "string"
 
 int main()
 {
-	ifstream in("f.txt");
-	ofstream out("g.txt");
-	string s1,s2;
-	int m = 0, k = 0, i = 0,e;
-	while (in.peek() != EOF)
-	{
-		getline(in, s1);
-		getline(in, s2);
-		out << (atoi(s1.c_str()) + atoi(s2.c_str()))/2. << endl;
+    ifstream in("f.txt");
+    ofstream out("g.txt");
+    string s1,s2;
+   
+    while (in.peek() != EOF)
+    {
+        getline(in, s1);
+        getline(in, s2);
+        out << (atoi(s1.c_str()) + atoi(s2.c_str()))/2. << endl;
 
 
-	}
-	in.close();
-	out.close();
-	return 0;
+    }
+    in.close();
+    out.close();
+    return 0;
 }
