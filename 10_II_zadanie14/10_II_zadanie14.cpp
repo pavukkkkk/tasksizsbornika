46663151d9144031501b9ca2ff3944b8c3d0f4a7
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 01:14:19 +0400

    Update 10_II_zadanie14.cpp

--- 10_II_zadanie14/10_II_zadanie14.cpp
+++ 10_II_zadanie14/10_II_zadanie14.cpp
@@ -1,31 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-#include "fstream";
-#include "string";
-#include "cmath";
+#include "fstream"
+#include "string"
+#include "cmath"
 struct vec {
-	string nazv;
-	double cena, nizh, verh;
-	double f(int n) {
-		return cena - cena / 100 * n;
+    string nazv;
+    double cena, nizh, verh;
+    double f(int n) {
+        return cena - cena / 100 * n;
 
-	}
+    }
 };
 int main()
 {
-	vec* a = new vec[1000000];
-	ifstream in("input.txt");
-	ofstream out("output.txt");
-	int i = 0;
-	int n;
-	cout << "vvedite x%: ";
-	cin >> n;
-	while (!in.eof()) {
-		in >> a[i].nazv >> a[i].cena >> a[i].nizh >> a[i].verh;
-		out<< a[i].nazv <<" "<<  a[i].f(n)<<" " << a[i].nizh<<" " << a[i].verh << endl;
-		i++;
-	}
-	in.close();
-	out.close();
-	return 0;
+    vec* a = new vec[1000000];
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int i = 0;
+    int n;
+    cout << "vvedite x%: ";
+    cin >> n;
+    in >> a[i].nazv >> a[i].cena >> a[i].nizh >> a[i].verh;
+    while (!in.eof()) {
+        out<< a[i].nazv <<" "<<  a[i].f(n)<<" " << a[i].nizh<<" " << a[i].verh << endl;
+        i++;
+        in >> a[i].nazv >> a[i].cena >> a[i].nizh >> a[i].verh;
+       
+    }
+    in.close();
+    out.close();
+    return 0;
 }
