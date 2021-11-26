599bf033ff34813578e54df82d11495b84373d75
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 10:51:11 +0400

    Update 6_I_zadanie17.cpp

--- 6_I_zadanie17/6_I_zadanie17.cpp
+++ 6_I_zadanie17/6_I_zadanie17.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int n, a,g,v;
+    int n, a,v;
     long long s = 1;
     cin >> v;
     if (v == 0) {
@@ -31,7 +31,7 @@ int main()
         int** w = new int* [n];
         for (int i = 0; i < n; i++) {
             w[i] = new int[u];
-            for (int j = 0; j < u; j++) {  
+            for (int j = 0; j < u; j++) {
                 cin >> w[i][j];
             }
         }
@@ -42,11 +42,24 @@ int main()
             }
         }
     }
-    if (s > 99 && s < 1000) {
+    if (abs(s) > 99 && abs(s) < 1000) {
         cout << "YES";
     }
     else {
         cout << "NO";
     }
 }
-
+/*
+ 
+ vvedite kolichestvo strochek: 2
+ vvedite kolichestvo stolbcov: 3
+ vvedite elementi :
+ 1 1 1 -123 1 1
+ YES
+ 
+ vvedite kolichestvo strochek: 2
+ vvedite kolichestvo stolbcov: 1
+ vvedite elementi :
+ 2 49
+ NO
+ */
