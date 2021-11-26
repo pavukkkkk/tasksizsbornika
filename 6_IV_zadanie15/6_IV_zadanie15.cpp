a82fceff5c250e3cf9ac6791ef9ab35957b722a0
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 11:12:24 +0400

    Update 6_IV_zadanie15.cpp

--- 6_IV_zadanie15/6_IV_zadanie15.cpp
+++ 6_IV_zadanie15/6_IV_zadanie15.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
 
@@ -14,6 +14,16 @@ int** vvod(int n) {
     return m;
 }
 
+void vivod(int** m, int n) {
+    cout << "novaya matrica:  " << endl;;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << m[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n,k1,k2,s;
@@ -22,6 +32,8 @@ int main()
     cout << "vvedite nomer k1 & k2(nomera vkluchitelno): ";
     cin >> k1 >> k2;
     int** m = vvod(n);
+    cout<<"Ishodnaya matrica: \n";
+    vivod(m,n);
     int* d = new int[n];
     for (int j = 0; j < n; j++) {
         s = 1;
@@ -43,3 +55,39 @@ int main()
     delete[] m;
     delete[] d;
 }
+/*
+ vvedite razmer marici n: 3
+ vvedite nomer k1 & k2(nomera vkluchitelno): 1 2
+ vvedite maricu:
+ 1 2 3
+ 4 5 6
+ 7 8 9
+ Ishodnaya matrica:
+ novaya matrica:
+ 1    2    3
+ 4    5    6
+ 7    8    9
+ massiv proizvedenyi: 4 10 18
+ vvedite razmer marici n: 3
+ vvedite nomer k1 & k2(nomera vkluchitelno): 1 1
+ vvedite maricu:
+ 1 2 3
+ 4 5 6
+ 7 8 9
+ Ishodnaya matrica:
+ novaya matrica:
+ 1    2    3
+ 4    5    6
+ 7    8    9
+ massiv proizvedenyi: 1 2 3
+ vvedite razmer marici n: 2
+ vvedite nomer k1 & k2(nomera vkluchitelno): 1 2
+ vvedite maricu:
+ 0 1
+ 2 0
+ Ishodnaya matrica:
+ novaya matrica:
+ 0    1
+ 2    0
+ massiv proizvedenyi: 0 0
+ */
