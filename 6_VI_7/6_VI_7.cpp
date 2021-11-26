63530c5ef4120f3e23d828692fc7df77feb87368
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-11-26 11:30:25 +0400

    Update 6_VI_7.cpp

--- 6_VI_7/6_VI_7.cpp
+++ 6_VI_7/6_VI_7.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
 int** vvod(int n,int k) {
@@ -14,10 +14,10 @@ int** vvod(int n,int k) {
 }
 
 void vivod(int** m, int n, int k) {
-    cout << "novaya matrica:  " << endl;;
+    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < k; j++) {
-            cout << m[i][j] << " ";
+            cout << m[i][j] << "\t";
         }
         cout << endl;
     }
@@ -26,11 +26,13 @@ void vivod(int** m, int n, int k) {
 
 int main()
 {
-    int n, k, p, a = 0, b;
+    int n, k, p, a = 0;
     cout << "vvedite razmer matrici: ";
     cin >> n >> k;
     int** m = vvod(n, k);
-    int d;
+    cout << "matrica:  " << endl;
+    vivod(m, n, k);
+
     int j = 0;
     while (j < k) {
         if (m[0][j] > m[n - 1][j]) {
@@ -46,7 +48,7 @@ int main()
         }
 
     }
-
+    cout << "novaya matrica:  " << endl;
     vivod(m, n, k);
     for (int i = 0; i < n; i++) {
         delete[] m[i];
@@ -54,3 +56,48 @@ int main()
     delete[] m;
 
 }
+/*
+ vvedite razmer matrici: 3 3
+ vvedite maricu:
+ 1 2 3
+ 4 5 6
+ 7 8 9
+ matrica:
+ 1    2    3
+ 4    5    6
+ 7    8    9
+ novaya matrica:
+ 1    2    3
+ 4    5    6
+ 7    8    9
+ */
+/*
+ vvedite razmer matrici: 3 4
+ vvedite maricu:
+ 1 2 3 4
+ 8 7 6 5
+ 9 10 11 12
+ matrica:
+ 1    2    3    4
+ 8    7    6    5
+ 9    10    11    12
+ novaya matrica:
+ 1    2    3    4
+ 8    7    6    5
+ 9    10    11    12
+ */
+/*
+ vvedite razmer matrici: 3 3
+ vvedite maricu:
+ 1 7 3
+ 0 0 0
+ 3 2 5
+ matrica:
+ 1    7    3
+ 0    0    0
+ 3    2    5
+ novaya matrica:
+ 1    3
+ 0    0
+ 3    5
+ */
